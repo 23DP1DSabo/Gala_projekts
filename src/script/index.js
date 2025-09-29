5212d8cdd07e2d04c57098d41adca5ea8bd691a2
Daniels <a230158ds@rvt.lv>
2025-09-29 10:47:33 +0300

    Made theme selector, improved code readability, changed grid to flexbox in navigation.

---
+++ src/script/index.js
@@ -0,0 +1,11 @@
+// Themes
+
+function lightMode() {
+    document.body.classList.add("light-mode");
+    document.body.classList.remove("dark-mode");
+  }
+  
+  function darkMode() {
+    document.body.classList.remove("light-mode");
+    document.body.classList.add("dark-mode");
+  }
