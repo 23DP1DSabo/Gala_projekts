3c4377c8c5c94e0f9c385a2c037264d734c91a30
Daniels <a230158ds@rvt.lv>
2025-09-30 11:29:19 +0300

    tweaked some things

--- src/script/index.js
+++ src/script/index.js
@@ -2,10 +2,14 @@
 
 function lightMode() {
     document.body.classList.add("light-mode");
-    document.body.classList.remove("dark-mode");
   }
   
-  function darkMode() {
+function darkMode() {
     document.body.classList.remove("light-mode");
-    document.body.classList.add("dark-mode");
   }
+
+
+// Load on home
+window.onload = function() {
+    window.location.hash = "#home";
+};
