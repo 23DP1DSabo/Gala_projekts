29d7439506c6cd516d5cbc820a3b0f9d23488dbc
Daniels <a230158ds@rvt.lv>
2025-10-07 11:23:39 +0300

    modified all files

--- src/script/index.js
+++ src/script/index.js
@@ -2,11 +2,28 @@
 
 function lightMode() {
     document.body.classList.add("light-mode");
+    document.body.classList.remove("dark-mode");
   }
   
 function darkMode() {
     document.body.classList.remove("light-mode");
+    document.body.classList.add("dark-mode");
   }
+
+
+// Languages
+
+function latvianMode() {
+  // TODO
+}
+
+function englishMode() {
+  // TODO
+}
+
+function italianMode() {
+  // TODO
+}
 
 
 // Load on home
