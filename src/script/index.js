c9c71b9a7a68858cf95bd9f1e7d533fc57e46c57
Daniels <a230158ds@rvt.lv>
2025-10-14 11:23:38 +0300

    completed light theme

--- src/script/index.js
+++ src/script/index.js
@@ -1,13 +1,11 @@
 // Themes
 
 function lightMode() {
-    document.body.classList.add("light-mode");
-    document.body.classList.remove("dark-mode");
+    document.body.classList.toggle("light-mode");
   }
   
 function darkMode() {
-    document.body.classList.remove("light-mode");
-    document.body.classList.add("dark-mode");
+    document.body.classList.toggle("dark-mode");
   }
 
 
